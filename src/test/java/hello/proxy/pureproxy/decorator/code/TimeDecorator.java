c19decc6236c6276338d9faf1c1046cdd8c02429
ydh6226 <ydh6226@naver.com>
2022-01-03 20:16:20 +0900

    데코레이터 패턴 부가기능 여러개 적용

---
+++ src/test/java/hello/proxy/pureproxy/decorator/code/TimeDecorator.java
@@ -0,0 +1,23 @@
+package hello.proxy.pureproxy.decorator.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TimeDecorator implements Component {
+
+    private final Component target;
+
+    public TimeDecorator(Component target) {
+        this.target = target;
+    }
+
+    @Override
+    public String operation() {
+        log.info("TimeDecorator 실행");
+        long startTime = System.currentTimeMillis();
+        String result = target.operation();
+        long endTime = System.currentTimeMillis();
+        log.info("TimeDecorator 종료. resultTime: {}", endTime - startTime);
+        return result;
+    }
+}
