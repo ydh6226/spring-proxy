9d71362fb4b9c908c9d880bf25ef35b03dc4cec0
ydh6226 <ydh6226@naver.com>
2022-01-03 19:52:31 +0900

    프록시 예제2

---
+++ src/test/java/hello/proxy/pureproxy/proxy/code/CacheProxy.java
@@ -0,0 +1,23 @@
+package hello.proxy.pureproxy.proxy.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CacheProxy implements Subject {
+
+    private Subject target;
+    private String cachedValue;
+
+    public CacheProxy(Subject target) {
+        this.target = target;
+    }
+
+    @Override
+    public String operation() {
+        log.info("프록시 호출");
+        if (cachedValue == null) {
+            cachedValue = target.operation();
+        }
+        return cachedValue;
+    }
+}
